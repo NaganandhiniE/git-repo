def6ec7de25c54f69af2ae4c365000866ed5f9a3
NaganandhiniE <42062566+NaganandhiniE@users.noreply.github.com>
2020-10-16 10:34:03 +0530

    deleted productQuantity thr br1

--- OnlineShoppingN/src/com/lti/model/Product.java
+++ OnlineShoppingN/src/com/lti/model/Product.java
@@ -4,10 +4,7 @@ public class Product {
   int productId;
   String productName;
   String productDescription;
-int productQuantity;
-	public int getproductQuantity{
-		return productQuantity;
-	}
+
 	
 public int getProductId() {
 	return productId;
